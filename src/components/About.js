8d635aabb59546cc239cc77b60bac15a9a9b5111
Dmitry <dmitry2595@gmail.com>
2025-04-15 12:13:11 +0300

    feat: add small screen options

--- src/components/About.js
+++ src/components/About.js
@@ -7,6 +7,7 @@ export const About = () => {
         <img src={example}></img>
         <h1>upload .csv file → set parameters → save the result</h1>
         <div>Any questions? <a href='mailto:dmitryletov2595@ya.ru'>Contact me</a></div>
+        <span className='small-width-description'>Sorry, it seems that your device screen width is too low. Make sure, that your device screen width &gt; 800 pixels for correct working.</span>
     </div>
   )
 }

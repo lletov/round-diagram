3151e641200b48cc2aae98414240695cda9fbe81
Dmitry <dmitry2595@gmail.com>
2025-06-11 12:31:54 +0300

    fix: adit tooltip text

--- src/components/FileTooltip.js
+++ src/components/FileTooltip.js
@@ -27,7 +27,7 @@ export const FileTooltip = () => {
         >
             <div className='panel-block-header'>
                 <h1>Tooltip</h1>
-                <h2>Here the file structure sample is shown. Make your files with the same structure. Copy it, paste in text editor, edit as you need with your points and connections, save as .csv file, upload here with file uploader and see the result.</h2>
+                <h2>Here the file structure sample is shown. Make your files with the same structure. You can copy it & paste in text editor, or download same sample, edit as you need with your points and connections, save as .csv file, upload here with file uploader and see the result.</h2>
             </div>
             <FileSample/>
         </div>

26cdb7c439df4d395777ca4c41d398eaf78d3a7d
Dmitry <dmitry2595@gmail.com>
2025-06-11 12:00:29 +0300

    feat: add download sample option

--- src/components/FileSample.js
+++ src/components/FileSample.js
@@ -18,16 +18,20 @@ export default function FileSample() {
     <div className='file-sample'>
         <div className='file-sample-header'>
             <h1>File sample</h1>
-            <button className='btn-s' onClick={handleClick}>{btnText}</button>
+            <div className='file-panel-header'>
+              <button className='btn-s' onClick={handleClick}>{btnText}</button> 
+              <a href="https://lletov.github.io/round-diagram/data.csv" download>
+                <button className='btn-s'>Download</button> 
+              </a>
+            </div>
         </div>
         <div className='file-sample-code' id='file-sample-code'>
         Title,Point,Group,Contacts<br/>
-        'Point 1',point1,group1,"point2; point4"<br/>
-        'Point 2',point2,group1,""<br/>
-        'Point 3',point3,group2,"point6"<br/>
-        'Point 4',point4,group2,""<br/>
-        'Point 5',point5,group3,""<br/>
-        'Point 6',point6,group3,""<br/>
+        Point 1,point1,group1,"point2"<br/>
+        Point 2,point2,group2,""<br/>
+        Point 3,point3,group3,""<br/>
+        Point 4,point4,group3,"point3; point5"<br/>
+        Point 5,point5,group4,""<br/>
         </div>
     </div>
   )
